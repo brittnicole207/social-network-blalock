66891d094550b6f782612780b6f0563a84fd3bbd
BRITTANY BLALOCK <brittany.blalock2@gmail.com>
2022-12-02 09:23:39 -0800

    gitignore

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose'); 
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urelencoded({ extended: true })); 
+app.use(express.urelencoded({ extended: true }));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || mongodb://localhost/social-network', {
-    useFindAndModify: false; 
-    useNewUrlParser: true; 
-    useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
-app.listen(PORT, () => console.log(`Connect to localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Connect to localhost:${PORT}`));
